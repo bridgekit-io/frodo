1d526817fd349e72b7f6f1643de32ed85429c2d2
rob.signorelli <rob.signorelli@gmail.com>
2025-03-19 15:20:44 -0400

    Added ping/pong heartbeat support to websockets.

--- internal/quiet/quiet.go
+++ internal/quiet/quiet.go
@@ -7,6 +7,14 @@ import "io"
 // trying to handle errors doesn't make a damn bit of difference.
 func Close(closer io.Closer) {
 	if closer != nil {
-		_ = closer.Close()
+		IgnoreError(closer.Close())
 	}
+}
+
+// IgnoreError is an explict way to indicate that you're performing some sort of action where
+// you really don't care about the error it can generate. Use this sparingly! Most errors
+// should be handled, but if you're trying to close a file right before exiting the program,
+// it may not be the end of the world to give a few less fucks about that error.
+func IgnoreError(_ error) {
+	// do nothing...
 }
